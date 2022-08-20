6a4e44a167ec2c65f7a1f5a6942277da31e6c40a
luiz.lima@live.com <luiz.lima@live.com>
2022-08-19 21:59:48 -0300

    add: get workitem name

--- feature/src/App.tsx
+++ feature/src/App.tsx
@@ -5,46 +5,51 @@ import {
   WorkItemTrackingServiceIds
 } from "azure-devops-extension-api/WorkItemTracking";
 
-
-import { GitRepository } from 'azure-devops-extension-api/Git';
-
 import './app.scss';
 
-import { Link } from '@fluentui/react/lib/Link';
 import { Card } from "azure-devops-ui/Card";
 import { Button } from "azure-devops-ui/Button";
-import { Dialog } from "azure-devops-ui/Dialog";
 import { Observer } from "azure-devops-ui/Observer";
 import { ObservableValue } from "azure-devops-ui/Core/Observable";
 import { Dropdown } from "azure-devops-ui/Dropdown";
-import { dropdownItems } from "./services/data";
-import { DropdownSelection } from "azure-devops-ui/Utilities/DropdownSelection";
+import { ButtonGroup } from "azure-devops-ui/ButtonGroup";
+import { IListBoxItem } from 'azure-devops-ui/ListBox';
+import { Icon } from 'azure-devops-ui/Icon';
 
 import { CreateFeatureBranchAsync, GetRepositoriesAsync } from './services/repository';
-import { IListBoxItem } from 'azure-devops-ui/ListBox';
-
 
 interface IAppState {
-  page: string;
+  repository: string;
 }
 
 class App extends React.Component<{}, IAppState>  {
-  
+
   workItemFormService = DevOps.getService<IWorkItemFormService>(
     WorkItemTrackingServiceIds.WorkItemFormService
   );
 
   private repositories: Array<IListBoxItem<{}>> = [];
-  private selection = new DropdownSelection();
   private isDialogOpen = new ObservableValue<boolean>(false);
 
   constructor(props: {}) {
     super(props);
-    this.selection.select(0);
+
+    this.state = {
+      repository: ""
+    }
+
+    this.workItemFormService.then(currentItem => {
+      currentItem.getId().then(id => {
+        console.log(id)
+      });
+      currentItem.getFields().then(fields => {
+        console.log(fields)
+      });
+    });
 
     GetRepositoriesAsync().then(items => {
       items.forEach(element => {
-        this.repositories.push({id: element.name, text: element.name, iconProps: { iconName: "GitLogo", style: { color: "#f05133" } }})
+        this.repositories.push({ id: element.name, text: element.name, iconProps: { iconName: "GitLogo", style: { color: "#f05133" } } })
       });
     });
   }
@@ -53,81 +58,95 @@ class App extends React.Component<{}, IAppState>  {
     DevOps.init();
   }
 
+  isValid(): boolean {
+    const { repository } = this.state;
+
+    return (
+      repository !== undefined && repository.trim() !== ""
+    );
+  }
+
   async create() {
-    console.log(this.selection)
-    //await CreateFeatureBranchAsync('Eleven.Tools.K8S', 'develop', 'ft#5555');
-    this.isDialogOpen.value = false;
+    if (this.state.repository) {
+      await CreateFeatureBranchAsync(this.state.repository, 'develop', 'ft#5555');
+      this.isDialogOpen.value = false;
+    }
   }
 
   render(): JSX.Element {
 
-    const onDismiss = () => {
-      this.isDialogOpen.value = false;
-    };
-
     return (
-      <div>
-        <Button
-          text="Create"
-          subtle={true}
-          onClick={() => this.isDialogOpen.value = true}
-        />
-        <Card className="flex-grow">
-          <p>Não existe uma branch vinculada a esse item, You can also    </p> <Link
-            className="scroll-hidden flex-row flex-baseline branch-link monospaced-text bolt-link subtle"
-            excludeTabStop
-            target="_blank"
-          >
-            develop
-          </Link> <p>to get started.</p>
-        </Card>
 
-        <Observer isDialogOpen={this.isDialogOpen}>
-          {(props: { isDialogOpen: boolean }) => {
-            return props.isDialogOpen ? (
-              <Observer isDialogOpen={this.isDialogOpen}>
-                {(props: { isDialogOpen: boolean }) => {
-                  return props.isDialogOpen ? (
-                    <Dialog
-                      titleProps={{ text: "Create a branch" }}
-                      footerButtonProps={[
-                        {
-                          text: "Cancel",
-                          onClick: onDismiss
-                        },
-                        {
-                          text: "Create",
-                          onClick: () => {this.create()},
-                          primary: true
-                        }
-                      ]}
-                      onDismiss={onDismiss} >
+      <Observer isDialogOpen={this.isDialogOpen}>
+        {(props: { isDialogOpen: boolean }) => {
+          return props.isDialogOpen ? (
+            <div className="branch--content">
+              <div className="branch--group">
+                <label className="branch--group-label">
+                  Repository *
+                </label>
+                <Dropdown
+                  ariaLabel="Basic"
+                  placeholder="Select a repository"
+                  showPrefix={true}
+                  items={this.repositories}
+                  onSelect={(event, item) => {
+                    console.log(item.id);
+                    this.setState({ repository: item.id });
+                  }}
+                />
+              </div>
+              <div className="branch--group">
+                <span className="fontSize font-size secondary-text flex-row flex-center text-ellipsis">
+                  {Icon({
+                    className: "icon-margin",
+                    iconName: "OpenSource",
+                    key: "branch-name",
+                  })}
+                  develop
+                </span>
+              </div>
 
-                      <div className="branch--content">
-                        <div className="branch--group flex-column">
-                          <label className="bolt-textfield-label" >Repository *</label>
-                          <Dropdown
-                            ariaLabel="Single select"
-                            className="example-dropdown"
-                            placeholder="Select an Option"
-                            showPrefix={true}
-                            items={this.repositories}
-                            selection={this.selection}
-                          />
-                        </div>
-                      </div>
+              <div className="branch--add-button">
+                <ButtonGroup>
+                  <Button
+                    text="Cancel"
+                    onClick={() => this.isDialogOpen.value = false}
+                  />
+                  <Button
+                    text="Create"
+                    primary={true}
+                    onClick={() => this.create()}
+                    disabled={!this.isValid()}
+                  />
+                </ButtonGroup>
+              </div>
+            </div>
+          ) : (
+            <div>
+              <Button
+                text="Create"
+                subtle={true}
+                onClick={() => this.isDialogOpen.value = true}
+              />
+              <Card>
+                There is no branch linked to this item. Click create to link.
+              </Card>
+            </div>
+          );
+        }}
+      </Observer>
 
-                      You have modified this work item. You can save your changes, discard
-                      your changes, or cancel to continue editing.
-                    </Dialog>
-                  ) : null;
-                }}
-              </Observer>
-            ) : null;
-          }}
-        </Observer>
 
-      </div>
+
+
+
+
+
+
+
+
+
     )
   }
 }
